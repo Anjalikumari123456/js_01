458043e3c92547b466c431c51459968db75d06d4
Anjali <anjalikumari27apr@gmail.com>
2024-12-10 14:25:27 +0530

    modified

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,4 +23,27 @@ console.log(booleanIsLoggedIn);
 //""=>false
 //"hitesh" =>true
 
+// **************** Operations ***********************
+
+let value=3
+let negValue=-value
+console.log(negValue)
+
+let str1="hello"
+let str2=" hitesh"
+
+let str3=str1+str2
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
 
