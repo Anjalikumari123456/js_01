b5f3b5e1de8f8555596be5df552dfe995a1f45c2
Anjali <anjalikumari27apr@gmail.com>
2024-12-10 14:05:17 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+accountEmail="hc@hc.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not tp use var for declaring a variable
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) -->we are using nodejs,not browser
+
+console.log(3+3)
+
+
+let name="anjali"
+let age=18
+let isLoggedIn=false
+
+//Types of datatype:
+
+// number=> 2 to the power 53
+// BigInt
+// string=>""
+// boolean==>true/false
+//null==>stanalone value
+//undefined
+//symbol=>inique
+
+//object
+
+console.log(typeof "hitesh");
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
+
+
+
